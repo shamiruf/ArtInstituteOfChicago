651ab99f3358159632a0d261e1fbb9712c9daa74
shamiruf <rufashamikh@gmail.com>
2022-11-17 22:56:42 +0100

    Initial project

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>Art Institute of Chicago</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
